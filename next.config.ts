14cc6208be3588fe413031426d46cdbc474ad3af
morningbeach <morningbeachtw@gmail.com>
2026-05-07 13:59:27 +0800

    fix: allow vercel blob remote pattern in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "host.docker.internal", "192.168.0.44"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
